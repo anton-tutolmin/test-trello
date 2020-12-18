2cdbb270567d569d558ba253d079fdfa4acdf779
Anton Tutolmin <crysisage@gmail.com>
2020-12-18 11:05:27 +0600

    init

--- src/comments/dto/update-comment.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
-
-export class UpdateCommentDto {
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  text: string;
-}
--- src/users/guards/user.guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
-
-@Injectable()
-export class UserGuard implements CanActivate {
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const req = context.switchToHttp().getRequest();
-    return req.params.id === req.user.id;
-  }
-}
